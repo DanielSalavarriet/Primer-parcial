d4edee3c23ec2fc54bbd7db2132b24be1a80a36a
[danie] <danielsantiagosalavarrietai@gmail.com>
2025-09-19 18:08:33 -0500

    Corrijo: agrego función main mínima para compilar

--- distancias.cpp
+++ distancias.cpp
@@ -1,1 +1,13 @@
 #include <iostream>
+
+// Estructura para un punto 2D
+struct Coordenada {
+    double posX;
+    double posY;
+};
+
+int main() {
+    std::cout << "Programa base creado correctamente.\n";
+    return 0;
+}
+#include <iostream>
