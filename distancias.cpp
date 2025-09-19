38ed536b89f256c4168815f3c4cf3506909edfd7
[danie] <danielsantiagosalavarrietai@gmail.com>
2025-09-19 18:21:42 -0500

    Implemento obtenerDistancia para calcular distancia euclidiana

--- distancias.cpp
+++ distancias.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 // Estructura para un punto 2D
 struct Coordenada {
@@ -6,8 +7,17 @@ struct Coordenada {
     double posY;
 };
 
+// Función para calcular la distancia euclidiana
+double obtenerDistancia(const Coordenada &a, const Coordenada &b) {
+    return std::sqrt(std::pow(b.posX - a.posX, 2) + std::pow(b.posY - a.posY, 2));
+}
+
 int main() {
-    std::cout << "Programa base creado correctamente.\n";
+    Coordenada p1{0, 0};
+    Coordenada p2{3, 4};
+
+    std::cout << "Distancia entre p1 y p2 = " 
+              << obtenerDistancia(p1, p2) << std::endl;
+
     return 0;
 }
-#include <iostream>
